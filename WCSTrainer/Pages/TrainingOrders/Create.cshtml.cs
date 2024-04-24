0d7977f8752e3a48ac647f5cab33027f10750b15
rownox <rownox.ik@gmail.com>
2024-04-24 15:42:34 -0400

    Layout Change 2

--- WCSTrainer/Pages/TrainingOrders/Create.cshtml.cs
+++ WCSTrainer/Pages/TrainingOrders/Create.cshtml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using WCSTrainer.Data;
 using WebApplication2.Models;
 
 namespace WCSTrainer.Pages.TrainingOrders
@@ -14,6 +9,10 @@ namespace WCSTrainer.Pages.TrainingOrders
     {
         private readonly WCSTrainer.Data.WCSTrainerContext _context;
 
+        public List<SelectListItem> Individuals { get; set; }
+        public List<SelectListItem> Groups { get; set; }
+        public List<SelectListItem> Locations { get; set; }
+
         public CreateModel(WCSTrainer.Data.WCSTrainerContext context)
         {
             _context = context;
@@ -21,13 +20,35 @@ namespace WCSTrainer.Pages.TrainingOrders
 
         public IActionResult OnGet()
         {
+            Individuals = new List<SelectListItem> {
+                new SelectListItem { Text = "Aadam Husain" },
+                new SelectListItem { Text = "Jay Dunwell" },
+                new SelectListItem { Text = "Craig Grambau" },
+                new SelectListItem { Text = "Donna Crowl" },
+                new SelectListItem { Text = "Kay Selim" }
+            };
+
+            Groups = new List<SelectListItem> {
+                new SelectListItem { Text = "HR" },
+                new SelectListItem { Text = "Sales" },
+                new SelectListItem { Text = "Shipping" },
+                new SelectListItem { Text = "Tooling" }
+            };
+
+            Locations = new List<SelectListItem> {
+                new SelectListItem { Text = "Four Slide Machine 1" },
+                new SelectListItem { Text = "Four Slide Machine 2" },
+                new SelectListItem { Text = "Four Slide Machine 3" },
+                new SelectListItem { Text = "Tool Room" },
+                new SelectListItem { Text = "The Office" }
+            };
+
             return Page();
         }
 
         [BindProperty]
         public TrainingOrder TrainingOrder { get; set; } = default!;
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
