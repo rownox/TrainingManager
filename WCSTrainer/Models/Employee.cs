d7593748cd03124f2ec6e3bc38244b5d9a25aa37
rownox <rownox.ik@gmail.com>
2024-05-07 13:16:46 -0400

    Completion

---
+++ WCSTrainer/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WCSTrainer.Models {
+    public class Employee {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Status { get; set; }
+        public string Trainings { get; set; }
+        public string Availability {  get; set; }
+    }
+}
---
+++ WCSTrainer/Models/Verification.cs
@@ -0,0 +1,9 @@
+﻿namespace WCSTrainer.Models {
+    public class Verification {
+        public int TrainingID { get; set; }
+        public string Completed {  get; set; }
+        public int VerifierID { get; set; }
+        public DateOnly VerificationDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
