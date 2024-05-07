2c8e6bd84edd2d347000cf9620649303f406c20f
WolverineAH <ahusain@wolverinecoilspring.com>
2024-05-07 13:16:46 -0400

    Completion

---
+++ WCSTrainer/Models/Trainer.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication2.Models;
+
+namespace WCSTrainer.Models {
+    public class Trainer {
+        public int EmployeeID { get; set; }
+        public string Skills { get; set; }
+        public List<int> Underlings { get; set; }
+        public List<int> Completed { get; set; } 
+    }
+}
--- WCSTrainer/Pages/TrainingOrders/Index.cshtml.cs
+++ WCSTrainer/Pages/TrainingOrders/Index.cshtml.cs
@@ -12,52 +12,21 @@ namespace WCSTrainer.Pages.TrainingOrders {
         }
 
         public IList<TrainingOrder> TrainingOrder { get; set; } = default!;
-        public TrainingOrder deletionOrder { get; set; } = default!;
-
-        public int? idnum = null;
 
         public async Task OnGetAsync() {
             TrainingOrder = await _context.TrainingOrder.ToListAsync();
         }
 
-        public async void setDeletionOrder() {
-            if (idnum == null) {
-                return;
-            }
+        public async Task<IActionResult> DeleteTrainingOrderAsync(int id) {
+            var trainingOrder = await _context.TrainingOrder.FindAsync(id);
 
-            var trainingorder = await _context.TrainingOrder.FirstOrDefaultAsync(m => m.Id == idnum);
-
-            if (trainingorder == null) {
-                return;
-            } else {
-                deletionOrder = trainingorder;
-            }
-        }
-
-        public async Task<IActionResult> deleteOrder() {
-
-            setDeletionOrder();
-
-            int? id = null;
-            if (deletionOrder != null) {
-                id = deletionOrder.Id;
-            }
-            
-            if (id == null) {
+            if (trainingOrder == null) {
                 return NotFound();
             }
 
-            var trainingorder = await _context.TrainingOrder.FindAsync(id);
+            _context.TrainingOrder.Remove(trainingOrder);
+            await _context.SaveChangesAsync();
 
-            if (trainingorder != null) {
-                foreach (TrainingOrder order in _context.TrainingOrder) {
-                    if (order.Id == id) {
-                        deletionOrder = trainingorder;
-                        _context.TrainingOrder.Remove(deletionOrder);
-                        await _context.SaveChangesAsync();
-                    }
-                }
-            }
             return RedirectToPage("./Index");
         }
     }
