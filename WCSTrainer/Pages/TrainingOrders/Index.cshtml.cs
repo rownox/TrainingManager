477caa1bf4c3f100419b30b53295975bab8091a8
rownox <rownox.ik@gmail.com>
2024-05-20 16:29:37 -0400

    UI changes and verification stage

--- WCSTrainer/Pages/TrainingOrders/Index.cshtml.cs
+++ WCSTrainer/Pages/TrainingOrders/Index.cshtml.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using WCSTrainer.Models;
 using WebApplication2.Models;
 
 namespace WCSTrainer.Pages.TrainingOrders {
     public class IndexModel : PageModel {
         private readonly WCSTrainer.Data.WCSTrainerContext _context;
+        private readonly WCSTrainer.Data.EmployeeContext _context2;
 
-        public IndexModel(WCSTrainer.Data.WCSTrainerContext context) {
+        public IndexModel(WCSTrainer.Data.WCSTrainerContext context, WCSTrainer.Data.EmployeeContext context2) {
             _context = context;
+            _context2 = context2;
         }
 
         public IList<TrainingOrder> TrainingOrder { get; set; } = default!;
+        public IList<Employee> Employees { get; set; }
 
         public async Task OnGetAsync() {
+            Employees = await _context2.Employee.ToListAsync();
             TrainingOrder = await _context.TrainingOrder.ToListAsync();
         }
 
@@ -28,6 +33,15 @@ namespace WCSTrainer.Pages.TrainingOrders {
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
+        }
+
+        public string getEmployee(string traineeID) {
+            foreach(Employee employee in Employees) {
+                if (employee.Id.ToString().Equals(traineeID)) {
+                    return employee.FirstName + " " + employee.LastName;
+                }
+            }
+            return traineeID;
         }
     }
 }
