08f298f98c31e92744895e06eab4d6123a5cf142
rownox <rownox.ik@gmail.com>
2024-05-22 11:10:16 -0400

    Scheduling Search by Year

--- WCSTrainer/Pages/TrainingOrders/Create.cshtml.cs
+++ WCSTrainer/Pages/TrainingOrders/Create.cshtml.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using WCSTrainer.Data;
 using WCSTrainer.Models;
 using WebApplication2.Models;
