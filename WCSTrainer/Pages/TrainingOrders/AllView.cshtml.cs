133faba95248ada95acefe7ef72b0cc1b1124a6d
rownox <rownox.ik@gmail.com>
2024-05-29 13:48:00 -0400

    QOL + Better Lists + People Component Checks

--- WCSTrainer/Pages/TrainingOrders/AllView.cshtml.cs
+++ WCSTrainer/Pages/TrainingOrders/AllView.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Models;
@@ -11,9 +12,22 @@ namespace WCSTrainer.Pages.TrainingOrders {
         }
 
         public IList<TrainingOrder> TrainingOrder { get; set; } = default!;
+        public int listCount = 0;
+        [BindProperty]
+        public int maxCount { get; set; } = 15;
+        [BindProperty]
+        public DateOnly From { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        [BindProperty]
+        public DateOnly To { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
         public async Task OnGetAsync() {
             TrainingOrder = await _context.TrainingOrder.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            TrainingOrder = await _context.TrainingOrder.ToListAsync();
+
+            return Page();
         }
     }
 }
