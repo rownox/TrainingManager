03d72e371fa1ca0022357e2046b8645476c2eb8d
rownox <rownox.ik@gmail.com>
2024-06-03 12:50:20 -0400

    Authentication and User Accounts

---
+++ WCSTrainer/Models/UserAccount.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WCSTrainer.Models {
+    public class UserAccount : IdentityUser {
+        public string EmployeeID { get; set; }
+        public bool LightMode { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+}
