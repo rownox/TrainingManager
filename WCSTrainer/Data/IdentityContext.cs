bd353212684eeb81ab61582d55a3a62b0e3a381b
rownox <rownox.ik@gmail.com>
2024-06-03 12:50:20 -0400

    Authentication and User Accounts

---
+++ WCSTrainer/Data/IdentityContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WCSTrainer.Models;
+
+namespace WCSTrainer.Data {
+    public class IdentityContext : IdentityDbContext<UserAccount> {
+        
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName = "admin";
+
+            var trainer = new IdentityRole("trainer");
+            trainer.NormalizedName = "trainer";
+
+            var trainee = new IdentityRole("trainee");
+            trainee.NormalizedName = "trainee";
+
+            builder.Entity<IdentityRole>().HasData(admin, trainee, trainer);
+        }
+    }
+}
--- WCSTrainer/Program.cs
+++ WCSTrainer/Program.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity;
 using WCSTrainer.Data;
+using WCSTrainer.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<WCSTrainerContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("WCSTrainerContext") ?? throw new InvalidOperationException("Connection string 'WCSTrainerContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection") ?? throw new InvalidOperationException("Connection string 'WCSTrainerContext' not found.")));
+
+builder.Services.AddDefaultIdentity<UserAccount>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<IdentityContext>();
+builder.Services.AddDbContext<IdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection") ?? throw new InvalidOperationException("Connection string 'WCSTrainerContext' not found.")));
 
 var app = builder.Build();
 
