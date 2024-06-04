c4b09dc80869ffefd33085a0aed9ce6afc6299b0
rownox <rownox.ik@gmail.com>
2024-06-04 10:46:39 -0400

    ikyabms

--- WCSTrainer/Pages/Index.cshtml.cs
+++ WCSTrainer/Pages/Index.cshtml.cs
@@ -4,8 +4,11 @@ namespace WCSTrainer.Pages {
     public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger) {
+        private readonly WCSTrainer.Data.IdentityContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, WCSTrainer.Data.IdentityContext context) {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet() {
