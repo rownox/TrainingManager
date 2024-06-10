6384eb57450609c2338b1fc0d4553d4628d77947
rownox <rownox.ik@gmail.com>
2024-06-10 09:08:41 -0400

    Added Svelte 1

---
+++ WCSTrainer/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  let name = 'world';
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <input bind:value={name} placeholder="Enter your name">
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    max-width: 240px;
+  }
+
+  input {
+    margin-top: 1em;
+  }
+</style>
