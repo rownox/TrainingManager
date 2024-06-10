5a22bbe3d00849294e41f323c63837652105a711
rownox <rownox.ik@gmail.com>
2024-06-10 09:50:02 -0400

    h

--- WCSTrainer/rollup.config.js
+++ WCSTrainer/rollup.config.js
@@ -1,7 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from '@rollup/plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 import path from 'path';
 
