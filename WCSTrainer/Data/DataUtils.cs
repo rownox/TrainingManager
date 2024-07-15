a41c91b9e0d177105a66685b81fa5f314fb17ca5
rownox <rownox.ik@gmail.com>
2024-07-15 16:35:15 -0400

    works

--- WCSTrainer/Data/DataUtils.cs
+++ WCSTrainer/Data/DataUtils.cs
@@ -7,48 +7,32 @@ namespace WCSTrainer.Data {
     public class DataUtils {
         private readonly WCSTrainerContext _context;
 
-        private List<Location> Locations;
-
         public DataUtils(WCSTrainerContext context) {
             _context = context;
-            initVars();
-        }
-
-        public async void initVars() {
-            Locations = await _context.Locations.ToListAsync();
         }
 
         public async Task<Employee?> GetEmployeeById(int id) {
-            return await _context.Set<Employee>().FindAsync(id);
+            return await _context.Employees.FindAsync(id);
         }
 
         public async Task<Location?> GetLocationById(int id) {
-            return await _context.Set<Location>().FindAsync(id);
+            return await _context.Locations.FindAsync(id);
         }
 
         public async Task<Skill?> GetSkillById(int id) {
-            return await _context.Set<Skill>().FindAsync(id);
+            return await _context.Skills.FindAsync(id);
         }
 
         public async Task<TrainerGroup?> GetTrainerGroupById(int id) {
-            return await _context.Set<TrainerGroup>().FindAsync(id);
+            return await _context.TrainerGroups.FindAsync(id);
         }
 
         public async Task<TrainingOrder?> GetTrainingOrderById(int id) {
-            return await _context.Set<TrainingOrder>().FindAsync(id);
+            return await _context.TrainingOrders.FindAsync(id);
         }
 
         public async Task<Verification?> GetVerificationById(int id) {
-            return await _context.Set<Verification>().FindAsync(id);
-        }
-
-        public Location? GetLocationFromId(int id) {
-            foreach (var loc in Locations) {
-                if (loc.Id == id) {
-                    return loc;
-                }
-            }
-            return null;
+            return await _context.Verifications.FindAsync(id);
         }
     }
 }
