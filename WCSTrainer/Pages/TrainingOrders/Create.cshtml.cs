f2d5c766ad8634320f10449dee2ff393880673d2
rownox <rownox.ik@gmail.com>
2024-07-15 16:51:48 -0400

    Training Order Creation Done

--- WCSTrainer/Pages/TrainingOrders/Create.cshtml.cs
+++ WCSTrainer/Pages/TrainingOrders/Create.cshtml.cs
@@ -20,14 +20,37 @@ namespace WCSTrainer.Pages.TrainingOrders {
 
         public DateOnly Day { get; } = DateOnly.FromDateTime(DateTime.Now);
         public List<int> newList = new List<int>();
+        public SelectList Locations { get; set; }
+
+        public IList<Employee> Employees { get; set; }
+        public IList<TrainerGroup> TrainerGroups { get; set; }
 
         public async Task<IActionResult> OnGetAsync() {
+
+            var locations = await _context.Locations.ToListAsync();
+            Locations = new SelectList(locations, "Id", "Name");
+
+            Employees = await _context.Employees.ToListAsync();
+            ViewData["EmployeesJson"] = System.Text.Json.JsonSerializer.Serialize(Employees ?? new List<Employee>());
+
+            TrainerGroups = await _context.TrainerGroups.ToListAsync();
+            ViewData["TrainerGroupsJson"] = System.Text.Json.JsonSerializer.Serialize(TrainerGroups ?? new List<TrainerGroup>());
+
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync() {
 
             if (!ModelState.IsValid) {
+                var locations = await _context.Locations.ToListAsync();
+                Locations = new SelectList(locations, "Id", "Name");
+
+                Employees = await _context.Employees.ToListAsync();
+                ViewData["EmployeesJson"] = System.Text.Json.JsonSerializer.Serialize(Employees ?? new List<Employee>());
+
+                TrainerGroups = await _context.TrainerGroups.ToListAsync();
+                ViewData["TrainerGroupsJson"] = System.Text.Json.JsonSerializer.Serialize(TrainerGroups ?? new List<TrainerGroup>());
+
                 return Page();
             }
 
