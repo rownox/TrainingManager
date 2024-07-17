7905524c8dda82915564036d6a807a6ee8042e18
rownox <rownox.ik@gmail.com>
2024-07-17 08:45:51 -0400

    Explicit types working fully

--- WCSTrainer/Pages/TrainingOrders/Details.cshtml.cs
+++ WCSTrainer/Pages/TrainingOrders/Details.cshtml.cs
@@ -7,34 +7,29 @@ using WCSTrainer.Models;
 namespace WCSTrainer.Pages.TrainingOrders {
     public class DetailsModel : PageModel {
         private readonly WCSTrainerContext _context;
-        private readonly DataUtils _dataUtils;
 
-        public DetailsModel(WCSTrainerContext context, DataUtils dataUtils) {
+        public DetailsModel(WCSTrainerContext context) {
             _context = context;
-            _dataUtils = dataUtils;
         }
 
         public TrainingOrder TrainingOrder { get; set; } = default!;
-        public IList<Employee> Employees { get; set; }
-        public Employee Trainee { get; set; }
-        public Location Location { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id) {
-            Employees = await _context.Employees.ToListAsync();
 
             if (id == null) {
                 return NotFound();
             }
 
-            var trainingorder = await _context.TrainingOrders.FirstOrDefaultAsync(m => m.Id == id);
+            var trainingorder = await _context.TrainingOrders
+                .Include(t => t.Trainee)
+                .Include(l => l.Location)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
             if (trainingorder == null) {
                 return NotFound();
             } else {
                 TrainingOrder = trainingorder;
             }
-
-            Location = await _dataUtils.GetLocationById(TrainingOrder.LocationId);
-            Trainee = await _dataUtils.GetEmployeeById(TrainingOrder.TraineeId);
 
             return Page();
         }
