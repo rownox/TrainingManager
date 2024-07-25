09bc28a4017240870312533be10577ecf69fc47a
rownox <rownox.ik@gmail.com>
2024-07-25 10:00:31 -0400

    Skill CRUD

---
+++ WCSTrainer/Pages/Skills/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WCSTrainer.Data;
+
+namespace WCSTrainer.Pages.Skills
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly WCSTrainer.Data.WCSTrainerContext _context;
+
+        public DeleteModel(WCSTrainer.Data.WCSTrainerContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Skill Skill { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (skill == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Skill = skill;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills.FindAsync(id);
+            if (skill != null)
+            {
+                Skill = skill;
+                _context.Skills.Remove(Skill);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
