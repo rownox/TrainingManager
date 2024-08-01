17b2996ed4460c971c7daaec728059da9b3a7286
rownox <rownox.ik@gmail.com>
2024-08-01 14:35:17 -0400

    h

--- WCSTrainer/Pages/Locations/Edit.cshtml.cs
+++ WCSTrainer/Pages/Locations/Edit.cshtml.cs
@@ -1,37 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using WCSTrainer.Data;
 
-namespace WCSTrainer.Pages.Locations
-{
-    public class EditModel : PageModel
-    {
+namespace WCSTrainer.Pages.Locations {
+    public class EditModel : PageModel {
         private readonly WCSTrainer.Data.WCSTrainerContext _context;
 
-        public EditModel(WCSTrainer.Data.WCSTrainerContext context)
-        {
+        public EditModel(WCSTrainer.Data.WCSTrainerContext context) {
             _context = context;
         }
 
         [BindProperty]
         public Location Location { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> OnGetAsync(int? id) {
+            if (id == null) {
                 return NotFound();
             }
 
-            var location =  await _context.Locations.FirstOrDefaultAsync(m => m.Id == id);
-            if (location == null)
-            {
+            var location = await _context.Locations.FirstOrDefaultAsync(m => m.Id == id);
+            if (location == null) {
                 return NotFound();
             }
             Location = location;
@@ -40,27 +28,19 @@ namespace WCSTrainer.Pages.Locations
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
                 return Page();
             }
 
             _context.Attach(Location).State = EntityState.Modified;
 
-            try
-            {
+            try {
                 await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!LocationExists(Location.Id))
-                {
+            } catch (DbUpdateConcurrencyException) {
+                if (!LocationExists(Location.Id)) {
                     return NotFound();
-                }
-                else
-                {
+                } else {
                     throw;
                 }
             }
@@ -68,8 +48,7 @@ namespace WCSTrainer.Pages.Locations
             return RedirectToPage("./Index");
         }
 
-        private bool LocationExists(int id)
-        {
+        private bool LocationExists(int id) {
             return _context.Locations.Any(e => e.Id == id);
         }
     }
