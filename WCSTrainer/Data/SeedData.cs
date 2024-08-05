54ac9ce0ef6ec81cfd0466f7fb43759ad74d9f46
rownox <rownox.ik@gmail.com>
2024-08-05 15:25:01 -0400

    h

--- WCSTrainer/Data/SeedData.cs
+++ WCSTrainer/Data/SeedData.cs
@@ -1,22 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
 namespace WCSTrainer.Data {
-  public static class SeedData {
-    public static async Task AssignRoles(UserManager<UserAccount> userManager, RoleManager<IdentityRole> roleManager) {
-      var user = await userManager.FindByEmailAsync("ahusain@wolverinecoilspring.com");
-      var user2 = await userManager.FindByEmailAsync("AadamH35@gmail.com");
+   public static class SeedData {
+      public static async Task AssignRoles(UserManager<UserAccount> userManager, RoleManager<IdentityRole> roleManager) {
+         var user = await userManager.FindByEmailAsync("ahusain@wolverinecoilspring.com");
+         var user2 = await userManager.FindByEmailAsync("AadamH35@gmail.com");
 
-      foreach (var account in userManager.Users.ToList()) {
-        await userManager.AddToRoleAsync(account, "trainee");
-      }
+         foreach (var account in userManager.Users.ToList()) {
+            await userManager.AddToRoleAsync(account, "trainee");
+         }
 
-      if (user != null) {
-        await userManager.AddToRoleAsync(user, "admin");
-      }
+         if (user != null) {
+            await userManager.AddToRoleAsync(user, "admin");
+         }
 
-      if (user2 != null) {
-        await userManager.AddToRoleAsync(user2, "trainer");
+         if (user2 != null) {
+            await userManager.AddToRoleAsync(user2, "trainer");
+         }
       }
-    }
-  }
+   }
 }
--- WCSTrainer/Models/TrainingOrder.cs
+++ WCSTrainer/Models/TrainingOrder.cs
@@ -1,21 +1,21 @@
 ﻿public class TrainingOrder {
-  public int Id { get; set; }
-  public DateOnly? BeginDate { get; set; }
-  public DateOnly? CompletionDate { get; set; }
-  public DateOnly CreateDate { get; set; }
-  public string Medium { get; set; }
-  public string Status { get; set; }
-  public int Duration { get; set; }
-  public string? Priority { get; set; }
-  public string Description { get; set; }
-  public string ClosingNotes { get; set; } = string.Empty;
-  public int TraineeId { get; set; }
-  public Employee? Trainee { get; set; }
-  public int LocationId { get; set; }
-  public Location? Location { get; set; }
-  public int? VerificationId { get; set; }
-  public Verification? Verification { get; set; }
-  public ICollection<Employee> Trainers { get; set; } = new List<Employee>();
-  public ICollection<TrainerGroup> TrainerGroups { get; set; } = new List<TrainerGroup>();
-  public ICollection<Skill> Skills { get; set; } = new List<Skill>();
+   public int Id { get; set; }
+   public DateOnly? BeginDate { get; set; }
+   public DateOnly? CompletionDate { get; set; }
+   public DateOnly CreateDate { get; set; }
+   public string Medium { get; set; }
+   public string Status { get; set; }
+   public int Duration { get; set; }
+   public string? Priority { get; set; }
+   public string Description { get; set; }
+   public string ClosingNotes { get; set; } = string.Empty;
+   public int TraineeId { get; set; }
+   public Employee? Trainee { get; set; }
+   public int LocationId { get; set; }
+   public Location? Location { get; set; }
+   public int? VerificationId { get; set; }
+   public Verification? Verification { get; set; }
+   public ICollection<Employee> Trainers { get; set; } = new List<Employee>();
+   public ICollection<TrainerGroup> TrainerGroups { get; set; } = new List<TrainerGroup>();
+   public ICollection<Skill> Skills { get; set; } = new List<Skill>();
 }
