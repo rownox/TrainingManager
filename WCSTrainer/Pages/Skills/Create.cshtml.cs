791db3774f35dded018bf8ecccb8a788ee09a5eb
rownox <rownox.ik@gmail.com>
2024-08-07 11:19:45 -0400

    h

--- WCSTrainer/Pages/Skills/Create.cshtml.cs
+++ WCSTrainer/Pages/Skills/Create.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace WCSTrainer.Pages.Skills {
    public class CreateModel : PageModel {
@@ -16,10 +17,22 @@ namespace WCSTrainer.Pages.Skills {
       [BindProperty]
       public Skill Skill { get; set; } = default!;
 
+      public string TrainingOrdersString { get; set; }
+
       // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
       public async Task<IActionResult> OnPostAsync() {
          if (!ModelState.IsValid) {
             return Page();
+         }
+
+         if (TrainingOrdersString != null) {
+            var trainingOrderIds = TrainingOrdersString.Split(", ").Select(int.Parse).ToList();
+            var orders = await _context.TrainingOrders
+                .Where(e => trainingOrderIds.Contains(e.Id))
+                .ToListAsync();
+            foreach (var i in orders) {
+               Skill.TrainingOrders.Add(i);
+            }
          }
 
          _context.Skills.Add(Skill);
