c6534f0ffac74a4633886a506f763f279ccb11f2
rownox <rownox.ik@gmail.com>
2024-08-13 12:43:49 -0400

    building pages

---
+++ WCSTrainer/Pages/Accounts/Details.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WCSTrainer.Pages.Accounts
+{
+    public class DetailsModel(UserManager<UserAccount> userManager) : PageModel
+    {
+      [BindProperty]
+      public UserAccount? UserAccount { get; set; }
+
+      public async Task<IActionResult> OnGetAsync(string id) {
+         UserAccount = await userManager.FindByIdAsync(id);
+         if (UserAccount == null) {
+            return NotFound();
+         }
+         return Page();
+      }
+
+      public async Task<IActionResult> OnPostAsync() {
+         if (UserAccount == null) {
+            return Page();
+         }
+
+         var user = await userManager.FindByIdAsync(UserAccount.Id);
+         if (user == null) {
+            return NotFound();
+         }
+         user.Email = UserAccount.Email;
+
+         var result = await userManager.UpdateAsync(user);
+         if (result.Succeeded) {
+            return RedirectToPage("Index");
+         }
+
+         foreach (var error in result.Errors) {
+            ModelState.AddModelError(string.Empty, error.Description);
+         }
+
+         return Page();
+      }
+   }
+}
