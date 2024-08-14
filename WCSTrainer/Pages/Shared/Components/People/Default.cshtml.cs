807faf2d24417e39e26aaf16c10a1edadbf266d8
rownox <rownox.ik@gmail.com>
2024-08-14 12:53:23 -0400

    hidden

--- WCSTrainer/Pages/Shared/Components/People/Default.cshtml.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace WCSTrainer.Pages.Shared.Components.People {
-   public class DefaultModel : PageModel {
-      public void OnGet() {
-      }
-   }
-}
