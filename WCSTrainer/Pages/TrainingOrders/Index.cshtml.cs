12f7ccbfe04e6d2b882467718ff68a708abbe319
rownox <rownox.ik@gmail.com>
2024-08-27 14:32:31 -0400

    dsfgd

--- WCSTrainer/Pages/TrainingOrders/Index.cshtml.cs
+++ WCSTrainer/Pages/TrainingOrders/Index.cshtml.cs
@@ -6,38 +6,36 @@ using Microsoft.EntityFrameworkCore;
 namespace WCSTrainer.Pages.TrainingOrders {
    [Authorize(Roles = "admin, trainer, trainee")]
 
-   public class IndexModel : PageModel {
-      private readonly WCSTrainer.Data.WCSTrainerContext _context;
-
-      public IndexModel(WCSTrainer.Data.WCSTrainerContext context) {
-         _context = context;
-      }
+   public class IndexModel(WCSTrainer.Data.WCSTrainerContext context) : PageModel {
 
       public IList<TrainingOrder> TrainingOrder { get; set; } = default!;
-      public IList<Employee> Employees { get; set; }
+      public IList<Employee> Employees { get; set; } = default!;
       public int listCount = 0;
+
       [BindProperty]
       public int maxCount { get; set; } = 10;
 
       public async Task OnGetAsync() {
-         Employees = await _context.Employees.ToListAsync();
-         TrainingOrder = await _context.TrainingOrders.ToListAsync();
+         Employees = await context.Employees.ToListAsync();
+         TrainingOrder = await context.TrainingOrders
+            .Include(t => t.Trainers)
+            .ToListAsync();
       }
 
       public async Task<IActionResult> OnPostAsync(int? id) {
          if (id != null) {
-            var trainingOrder = await _context.TrainingOrders.FindAsync(id);
+            var trainingOrder = await context.TrainingOrders.FindAsync(id);
 
             if (trainingOrder == null) {
                return NotFound();
             }
 
-            _context.TrainingOrders.Remove(trainingOrder);
-            await _context.SaveChangesAsync();
+            context.TrainingOrders.Remove(trainingOrder);
+            await context.SaveChangesAsync();
          }
 
-         Employees = await _context.Employees.ToListAsync();
-         TrainingOrder = await _context.TrainingOrders.ToListAsync();
+         Employees = await context.Employees.ToListAsync();
+         TrainingOrder = await context.TrainingOrders.ToListAsync();
 
          return Page();
       }
