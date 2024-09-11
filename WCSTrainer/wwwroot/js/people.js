86c251c437584f63603b74188e04cfa63f882085
rownox <rownox.ik@gmail.com>
2024-09-11 11:29:38 -0400

    h

--- WCSTrainer/wwwroot/js/people.js
+++ WCSTrainer/wwwroot/js/people.js
@@ -1,6 +1,4 @@
-var tempSelectedItems = [];
-
-function updateTrainersInput(input, output) {
+function updateInput(input, output) {
    var trainerListItems = Array.from(input.getElementsByTagName("li"));
    output.value = trainerListItems.map(item => `${item.getAttribute("value")}`).join(", ");
 }
@@ -8,27 +6,26 @@ function updateTrainersInput(input, output) {
 function removeItem(item) {
    if (item.tagName === "LI") {
       item.remove();
-      // updateTrainersInput();
-      // updateTraineeInput();
-      // updateGroupsInput();
+      // updateInput();
    }
 }
 
 function addItemToPartial(id, firstName, lastName) {
-   var exists = tempSelectedItems.some(function (item) {
-      return item.firstName == firstName && item.lastName == lastName;
+   var listElement = document.getElementById('partialTempList');
+   var exists = Array.from(listElement.children).some(function (li) {
+      return li.textContent === firstName + ' ' + lastName;
    });
 
    if (!exists) {
-      tempSelectedItems.push({ Id: id, firstName: firstName, lastName: lastName });
-
-      var listElement = document.getElementById('partialTempList');
       var li = document.createElement('li');
       li.classList.add("pill");
       li.addEventListener("click", function () {
          removeItem(li);
-      })
+      });
       li.textContent = firstName + ' ' + lastName;
+      li.dataset.id = id;
+      li.dataset.firstName = firstName;
+      li.dataset.lastName = lastName;
       listElement.appendChild(li);
    } else {
       alert(firstName + " " + lastName + " is already in the temporary list.");
@@ -36,18 +33,24 @@ function addItemToPartial(id, firstName, lastName) {
 }
 
 function confirmSelectionInPartial() {
-   confirmSelectionFromPartial(tempSelectedItems);
+   var listElement = document.getElementById('partialTempList');
+   var selectedItems = Array.from(listElement.children).map(function (li) {
+      return {
+         Id: li.dataset.id,
+         firstName: li.dataset.firstName,
+         lastName: li.dataset.lastName
+      };
+   });
 
-   tempSelectedItems = [];
-   document.getElementById('partialTempList').innerHTML = '';
+   confirmSelectionFromPartial(selectedItems);
+   listElement.innerHTML = '';
 }
 
 function confirmSelectionFromPartial(selectedItems) {
    var listElement = document.getElementById('trainerList');
-
    selectedItems.forEach(function (item) {
       var itemExistsInList = Array.from(listElement.children).some(function (li) {
-         return li.textContent == item.firstName + ' ' + item.lastName;
+         return li.textContent === item.firstName + ' ' + item.lastName;
       });
 
       if (!itemExistsInList) {
