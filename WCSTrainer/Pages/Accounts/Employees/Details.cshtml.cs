edccb28cde053f19261c1729a327d047c77c9050
rownox <rownox.ik@gmail.com>
2024-09-24 14:30:15 -0400

    people comp

---
+++ WCSTrainer/Pages/Accounts/Employees/Details.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WCSTrainer.Data;
+
+namespace WCSTrainer.Pages.Accounts.Employees {
+   public class DetailsModel(WCSTrainerContext context) : PageModel {
+
+      [BindProperty]
+      public Employee Employee { get; set; } = default!;
+
+      public async Task<IActionResult> OnGetAsync(int? id) {
+         var tempEmployee = await context.Employees.FirstOrDefaultAsync(a => a.Id == id);
+
+         if (tempEmployee != null) {
+            Employee = tempEmployee;
+         } else {
+            return NotFound();
+         }
+         return Page();
+      }
+   }
+}
