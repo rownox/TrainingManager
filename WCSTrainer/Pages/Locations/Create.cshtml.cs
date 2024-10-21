45bccd3f035e6bc5b44181270bc617a735fee41b
rownox <rownox.ik@gmail.com>
2024-10-21 07:24:09 -0400

    fixed location create

--- WCSTrainer/Pages/Locations/Create.cshtml.cs
+++ WCSTrainer/Pages/Locations/Create.cshtml.cs
@@ -4,13 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WCSTrainer.Pages.Locations {
    [Authorize(Roles = "owner, admin")]
-   public class CreateModel : PageModel {
-      private readonly WCSTrainer.Data.WCSTrainerContext _context;
-
-      public CreateModel(WCSTrainer.Data.WCSTrainerContext context) {
-         _context = context;
-      }
-
+   public class CreateModel(Data.WCSTrainerContext context) : PageModel {
       public IActionResult OnGet() {
          return Page();
       }
@@ -18,14 +12,13 @@ namespace WCSTrainer.Pages.Locations {
       [BindProperty]
       public Location Location { get; set; } = default!;
 
-      // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
       public async Task<IActionResult> OnPostAsync() {
          if (!ModelState.IsValid) {
             return Page();
          }
 
-         _context.Locations.Add(Location);
-         await _context.SaveChangesAsync();
+         context.Locations.Add(Location);
+         await context.SaveChangesAsync();
 
          return RedirectToPage("./Index");
       }
