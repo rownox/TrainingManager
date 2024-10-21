19a87d999d75eaed5c87e7342356e61c664c49ca
rownox <rownox.ik@gmail.com>
2024-10-21 08:09:30 -0400

    input fixes

--- WCSTrainer/Pages/Skills/Create.cshtml.cs
+++ WCSTrainer/Pages/Skills/Create.cshtml.cs
@@ -1,44 +1,41 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace WCSTrainer.Pages.Skills {
    [Authorize(Roles = "owner, admin")]
-   public class CreateModel : PageModel {
-      private readonly WCSTrainer.Data.WCSTrainerContext _context;
-
-      public CreateModel(WCSTrainer.Data.WCSTrainerContext context) {
-         _context = context;
-      }
-
+   public class CreateModel(Data.WCSTrainerContext context) : PageModel {
       [BindProperty]
       public Skill Skill { get; set; } = default!;
       [BindProperty]
       public string? LessonString { get; set; }
+      public SelectList LessonSelectList { get; set; }
 
-      public IActionResult OnGet() {
+      public async Task<IActionResult> OnGetAsync() {
+         LessonSelectList = new SelectList(await context.Lessons.ToListAsync(), "Id", "Name");
          return Page();
       }
 
       public async Task<IActionResult> OnPostAsync() {
-         //if (!ModelState.IsValid) {
-         //   return Page();
-         //}
+         if (!ModelState.IsValid) {
+            return Page();
+         }
 
-         _context.Skills.Add(Skill);
-         await _context.SaveChangesAsync();
+         context.Skills.Add(Skill);
+         await context.SaveChangesAsync();
 
          if (LessonString != null) {
             var lessonIds = LessonString.Split(", ").Select(int.Parse).ToList();
 
-            Skill.Lessons = await _context.Lessons
+            Skill.Lessons = await context.Lessons
                 .Where(e => lessonIds.Contains(e.Id))
                 .ToListAsync();
          }
 
-         _context.Skills.Update(Skill);
-         await _context.SaveChangesAsync();
+         context.Skills.Update(Skill);
+         await context.SaveChangesAsync();
 
          return RedirectToPage("./Index");
       }
