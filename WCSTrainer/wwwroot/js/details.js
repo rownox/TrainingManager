4970adef65ebb7d34d8a1a3d8b36144ba23ae928
rownox <rownox.ik@gmail.com>
2024-10-30 16:57:05 -0400

    Printing Spread

---
+++ WCSTrainer/wwwroot/js/details.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+   var printButton = document.getElementById('printButton');
+   if (printButton) {
+      printButton.addEventListener('click', function () {
+         window.print();
+      });
+   }
+});
