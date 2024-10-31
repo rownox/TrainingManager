7ca24096ab38deadd695ffba51182f1791dcb908
rownox <rownox.ik@gmail.com>
2024-10-31 14:38:36 -0400

    Detailed View Integration 3

--- WCSTrainer/Pages/TrainingOrders/Index.cshtml.cs
+++ WCSTrainer/Pages/TrainingOrders/Index.cshtml.cs
@@ -10,19 +10,28 @@ namespace WCSTrainer.Pages.TrainingOrders {
       public IList<TrainingOrder> TrainingOrder { get; set; } = default!;
       public IList<Employee> Employees { get; set; } = default!;
       public int listCount = 0;
-
+      [BindProperty]
       public int MaxCount { get; set; } = 10;
-
       [BindProperty]
       public bool Detailed { get; set; } = false;
 
       public async Task<IActionResult> OnGetAsync() {
+         if (TempData.ContainsKey("Detailed")) {
+            Detailed = (bool)TempData["Detailed"];
+         }
+         if (TempData.ContainsKey("MaxCount")) {
+            MaxCount = (int)TempData["MaxCount"];
+         }
+
          await LoadData();
          return Page();
       }
 
-      public IActionResult OnPostAsync() {
-         return OnGetAsync().Result;
+      public async Task<IActionResult> OnPostAsync() {
+         TempData["Detailed"] = Detailed;
+         TempData["MaxCount"] = MaxCount;
+
+         return RedirectToPage();
       }
 
       public async Task LoadData() {
