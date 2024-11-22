b7524624936d803c52718a38f9d6d1a54aa0ff74
rownox <rownox.ik@gmail.com>
2024-11-22 08:25:44 -0500

    Descirption Model

---
+++ WCSTrainer/Models/Description.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WCSTrainer.Models {
+   public class Description {
+      public int Id { get; set; }
+      public int PageId { get; set; }
+      public Lesson? Page { get; set; }
+      public ContentType Type { get; set; }
+      public string Content { get; set; } = string.Empty;
+      public byte[]? Data { get; set; }
+      public string? ContentType { get; set; } 
+      public int Row { get; set; }
+      public int Column { get; set; }
+   }
+}
+
+public enum ContentType {
+   Text,
+   Image,
+   Video
+}
--- WCSTrainer/Models/Lesson.cs
+++ WCSTrainer/Models/Lesson.cs
@@ -1,8 +1,11 @@
+using WCSTrainer.Models;
+
 public class Lesson {
    public int Id { get; set; }
    public string Name { get; set; } = string.Empty;
-   public string Description { get; set; } = string.Empty;
    public ICollection<TrainingOrder> TrainingOrders { get; set; } = [];
    public int LessonCategoryId { get; set; }
    public LessonCategory? LessonCategory { get; set; }
+   public int DescriptionId { get; set; }
+   public Description? Description { get; set; }
 }
