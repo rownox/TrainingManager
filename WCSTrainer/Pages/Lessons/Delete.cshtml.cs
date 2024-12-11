81a2badaf2269017fef021864326e4b4e56db1e6
rownox <rownox.ik@gmail.com>
2024-12-11 16:58:39 -0500

    Delete

--- WCSTrainer/Pages/Lessons/Delete.cshtml.cs
+++ WCSTrainer/Pages/Lessons/Delete.cshtml.cs
@@ -6,9 +6,9 @@ using Microsoft.EntityFrameworkCore;
 namespace WCSTrainer.Pages.Lessons {
    [Authorize(Roles = "owner")]
    public class DeleteModel : PageModel {
-      private readonly WCSTrainer.Data.WCSTrainerContext _context;
+      private readonly Data.WCSTrainerContext _context;
 
-      public DeleteModel(WCSTrainer.Data.WCSTrainerContext context) {
+      public DeleteModel(Data.WCSTrainerContext context) {
          _context = context;
       }
 
