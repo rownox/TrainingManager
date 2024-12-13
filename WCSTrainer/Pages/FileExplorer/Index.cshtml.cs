c8727f70c866c0a330420d18f8189de3e700e78c
rownox <rownox.ik@gmail.com>
2024-12-13 10:39:15 -0500

    Temporary File Explorer

--- WCSTrainer/Pages/FileExplorer/Index.cshtml.cs
+++ WCSTrainer/Pages/FileExplorer/Index.cshtml.cs
@@ -3,9 +3,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WCSTrainer.Pages.FileManager {
    [Authorize(Roles = "owner, admin")]
-   public class IndexModel : PageModel {
-      public void OnGet() {
+   public class IndexModel(IWebHostEnvironment webHostEnvironment) : PageModel {
 
+      public List<string> VideoFiles { get; set; } = new List<string>();
+
+      public void OnGet() {
+         string videoFolderPath = Path.Combine(webHostEnvironment.WebRootPath, "Shared", "Videos");
+
+         if (Directory.Exists(videoFolderPath)) {
+            var files = Directory.GetFiles(videoFolderPath);
+            foreach (var file in files) {
+               var relativePath = "/Shared/Videos/" + Path.GetFileName(file);
+               VideoFiles.Add(relativePath);
+            }
+         }
       }
    }
 }
