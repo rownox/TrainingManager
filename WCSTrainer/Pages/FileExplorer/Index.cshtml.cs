0849141d716a35a260c33f5cd53387ff2e969af9
rownox <rownox.ik@gmail.com>
2024-12-13 11:01:33 -0500

    File Explorer 2

--- WCSTrainer/Pages/FileExplorer/Index.cshtml.cs
+++ WCSTrainer/Pages/FileExplorer/Index.cshtml.cs
@@ -3,20 +3,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WCSTrainer.Pages.FileManager {
    [Authorize(Roles = "owner, admin")]
-   public class IndexModel(IWebHostEnvironment webHostEnvironment) : PageModel {
-
-      public List<string> VideoFiles { get; set; } = new List<string>();
+   public class IndexModel : PageModel {
 
       public void OnGet() {
-         string videoFolderPath = Path.Combine(webHostEnvironment.WebRootPath, "Shared", "Videos");
-
-         if (Directory.Exists(videoFolderPath)) {
-            var files = Directory.GetFiles(videoFolderPath);
-            foreach (var file in files) {
-               var relativePath = "/Shared/Videos/" + Path.GetFileName(file);
-               VideoFiles.Add(relativePath);
-            }
-         }
       }
    }
 }
